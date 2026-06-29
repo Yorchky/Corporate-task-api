17701a3b570ba1e8e6f0d9981357c90d92c65b8b
Yorchky <jorgeaag2005@gmail.com>
2026-06-29 09:25:10 -0600

    add environment configuration

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+require('dotenv').config();
+
 const express = require("express");
 
 const app = express(); // declararse ANTES de usarse
 const PORT = process.env.PORT || 5100;
+
+const connectDB = require("./src/config/database");
+
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Hello World. Server in running on port ${PORT}`);
