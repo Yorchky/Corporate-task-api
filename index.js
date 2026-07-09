91c1d7cc23b7cab9a354e58af5ef9d37443ecacb
Yorchky <jorgeaag2005@gmail.com>
2026-07-09 12:01:33 -0600

    Solve the missing routes of tasks and users in index

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const PORT = process.env.PORT || 5100;
 
 const connectDB = require("./src/config/database");
 
+const usuarioRoutes= require('./src/routes/usuarioRoutes');
 
+const tareaRoutes= require('./src/routes/tareaRoutes');
 connectDB();
+
+// MIDDLEWARE (IMPORTANTE)
+app.use(express.json());
+
+// ROUTES
+app.use('/api/usuario', usuarioRoutes);
+// ROUTES ACCOUNT
+app.use('/api/tareas', tareaRoutes);
 
 app.listen(PORT, () => {
   console.log(`Hello World. Server in running on port ${PORT}`);
