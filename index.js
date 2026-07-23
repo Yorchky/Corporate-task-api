c35503924e5346c9d22cdc4b89c8ee7077030431
Yorchky <jorgeaag2005@gmail.com>
2026-07-23 11:55:21 -0600

    helmet in vercel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require("express");
+<<<<<<< Updated upstream
 
 const app = express(); // declararse ANTES de usarse
 const PORT = process.env.PORT || 5100;
@@ -8,11 +9,26 @@ const PORT = process.env.PORT || 5100;
 const connectDB = require("./src/config/database");
 
 const usuarioRoutes= require('./src/routes/usuarioRoutes');
+=======
+const helmet = require("helmet") // implemented
+const app = express();
+
+const connectDB = require("./src/config/database");
+
+const PORT = process.env.PORT || 4000;
+
+const usuarioRoutes = require('./src/routes/usuarioRoutes');
+const tareaRoutes = require('./src/routes/tareaRoutes');
+>>>>>>> Stashed changes
 
 const tareaRoutes= require('./src/routes/tareaRoutes');
 connectDB();
 
+<<<<<<< Updated upstream
 // MIDDLEWARE (IMPORTANTE)
+=======
+app.use(helmet());
+>>>>>>> Stashed changes
 app.use(express.json());
 
 // ROUTES
@@ -24,6 +40,15 @@ if (process.env.VERCEL !== "1") {
   app.listen(PORT, () => {
     console.log(`Hello World. Server running on port ${PORT}`);
   });
+<<<<<<< Updated upstream
 }
+=======
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+>>>>>>> Stashed changes
 
 module.exports = app;
